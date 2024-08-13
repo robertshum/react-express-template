185d51fc0dec7560ba24978c5806cabd2336265b
robertshum <robertshum@gmail.com>
2024-08-13 15:36:44 -0700

    refactor edit form

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -1,18 +1,9 @@
 import { useForm } from "react-hook-form";
-import { usePizzasAPI, usePizzaMutationAPI } from '../hooks/useDataAPI';
+import { usePizzaMutationAPI } from '../hooks/useDataAPI';
 
 const Edit = () => {
 
   const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const {
-    dataFromQuery,
-    getError,
-    getLoading,
-  } = usePizzasAPI();
-
-  // console.log('datafromquery: ', dataFromQuery);
-  console.log('errors: ', errors);
 
   const {
     saveData,
@@ -22,51 +13,54 @@ const Edit = () => {
   } = usePizzaMutationAPI();
 
   const onSubmit = (data) => {
-    // event.preventDefault(); //RHF deals with this automatically.
-    console.log("data: ", data);
-    // TODO move this to a form, obviously.
+
+    //RHF deals with preventDefault behaviour automatically.
+
+    // Matches the schema in the backend
     const pizzaData =
     {
       name: data.pizzaName,
-      quantity: 3,
-      available: true
+      quantity: data.quantity,
+      available: data.availability
     };
-    saveData({ id: 1, data: pizzaData });
+    saveData({ id: data.id, data: pizzaData });
   };
 
-  if (getError) return <div><h1>Error Loading Data</h1></div>;
-  if (getLoading) return <div><h1>Loading Data...</h1></div>;
   if (saveError) return <div><h1>Error Saving Data</h1></div>;
   if (saveLoading) return <div><h1>Loading Save Data...</h1></div>;
 
   return (
     <div>
       <h1>Edit</h1>
-      <ul>
-        {dataFromQuery.map(pizza => (
-          <li key={pizza._id}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <label>Name of Entity</label>
-              {errors.pizzaName && <p>Please provide pizza name</p>}
-              <input
-                {...register('pizzaName', { required: true })}
-                type='text'
-                placeholder={pizza.name} />
-              <label>Quantity</label>
-              {errors.quantity && <p>Please provide quantity</p>}
-              <input
-                {...register('quantity', { required: true })}
-                type='number'
-                placeholder={pizza.quantity} />
-              <p>{pizza.available ? "Available" : "Not Available"}</p>
-              <button
-                type="submit"
-                onClick={handleSubmit(onSubmit)}>Save
-              </button>
-            </form>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>ID</label>
+        {errors.id && <p>Please provide id</p>}
+        <input
+          {...register('id', { required: true, valueAsNumber: true })}
+          type='number'
+          placeholder='1' />
+        <label>Name of Pizza</label>
+        {errors.pizzaName && <p>Please provide pizza name</p>}
+        <input
+          {...register('pizzaName', { required: true })}
+          type='text'
+          placeholder='Pizza Name' />
+        <label>Quantity</label>
+        {errors.quantity && <p>Please provide quantity</p>}
+        <input
+          {...register('quantity', { required: true, valueAsNumber: true })}
+          type='number'
+          placeholder='1' />
+        <label>Availability</label>
+        <input
+          {...register('availability')}
+          type='checkbox'
+        />
+        <button
+          type="submit"
+          onClick={handleSubmit(onSubmit)}>Save
+        </button>
+      </form>
     </div>
   );
 };
